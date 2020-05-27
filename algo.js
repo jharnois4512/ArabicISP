81507c5ecfbdd1d72d8b29796d95e8da8f11ae7b
Jeff <jharnois4512@gmail.com>
2020-05-27 17:29:15 -0400

    added part b and fixed some space input

--- algo.js
+++ algo.js
@@ -1,14 +1,27 @@
 
 exports.algo = function(x){
+  //error checking for a space
+  if(x.indexOf(' ') > 0){
+    x = x.substr(0, x.indexOf(' '));
+  }
   var len = x.length;
-  var holder = ''
+  var holder = []
+  holder = Array.from(x)
+
+  //creating a flag array of the size of the word to flag constant letters
+  var flagArr = []
+  flagArr.length = len
+  for(var i; i < len - 1; i++){
+    flagArr.push(0)
+  }
+  console.log(flagArr)
 
   //arrays for rules
   /*Part A - Normalization*/
   illegal = ['-', '?', '!', ',', '.', '/', '\\', '_', '#', '$', '(', ')']
-  /*Part B - list of constant letters*/
-  // constant = ['\u0631', '\u0632', '\u0634', '\u0635', '\u0636', '\u', '\u', '\u', '\u', '\u', '\u']
-  /*Part C - */
+  /*Part B - List of constant letters*/
+  constant = ['\u0631', '\u0632', '\u0634', '\u0635', '\u0636', '\u0637', '\u0638', '\u0639', '\u063A', '\u0642', '\u062F', '\u0630', '\u062B', '\u062C', '\u062D', '\u062E']
+  /*Part C - List of non-constant that might be converted*/
   prefix = ['\u0628', '\u0641', '\u0633', '\u0644']
   suffex = ['\uFEEB']
   prefixSuffex = ['\u0645', '\u0646', '\u0643']
@@ -16,12 +29,21 @@ exports.algo = function(x){
   extra = ['\u0629', '\uFE94']
   /*Part D - */
 
-  //searching for illegal characters
-  Array.from(x).forEach(letter => {
+  //execution of rules
+  /*searching for illegal characters - part A*/
+  holder.forEach(letter => {
     if(illegal.includes(letter)) throw new Error('Illegal character')
   });
-  return(x)
-  //TODO: Make list of constant letters
+  /*flagging constant letters - part B*/
+  for(var j = 0; j < len; j++){
+    if(constant.includes(holder[j])){
+      flagArr[j] = 1
+    }
+  }
+  console.log(flagArr)
+ /*TODO: write rule set for part C*/
 
+  //placeholder for testing the return for development
+  return(x)
 }
 
